c85d1ec7544cd909084afb2eeba031f2320c310e
Hassan Mehmood <ihassan463m@gmail.com>
2025-06-15 07:40:29 +0500

    adding

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-
-DATABASE_URL='postgresql://RAGBASED_owner:npg_mpfbTnJMB2R0@ep-polished-recipe-a8tkih10-pooler.eastus2.azure.neon.tech/RAGBASED?sslmode=require'
+DATABASE_URL=os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
